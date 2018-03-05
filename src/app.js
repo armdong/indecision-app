9025612e9bf9d580adb7321ce66971b0a2e0c4be
armdong <armdong@163.com>
2018-03-05 14:28:35 +0800

    complete indecision-app logic

--- src/app.js
+++ src/app.js
@@ -1,16 +1,59 @@
 
 class IndecisionApp extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      options: ['Thing one', 'Thing two', 'Thing three']
+    }
+
+    this.handleDeleteOptions = this.handleDeleteOptions.bind(this)
+    this.handlePick = this.handlePick.bind(this)
+    this.handleAddOption = this.handleAddOption.bind(this)
+  }
+
+  handleDeleteOptions() {
+    this.setState(() => ({
+      options: []
+    }))
+  }
+
+  handlePick() {
+    const randomNum = Math.floor(Math.random() * this.state.options.length)
+    const option = this.state.options[randomNum]
+    alert(option)
+  }
+
+  handleAddOption(option) {
+    if (!option) {
+      return 'Enter valid value to add item'
+    } else if (this.state.options.indexOf(option) > -1) {
+      return 'This option already exists'
+    }
+
+    this.setState((prevState) => ({
+      options: [...prevState.options, option]
+    }))
+  }
+
   render() {
     const title = "Indecision"
     const subtitle = "Put your life in the hands of a computer"
-    const options = ['thing one', 'thing two', 'thing three']
 
     return (
       <div>
         <Header title={title} subtitle={subtitle} />
-        <Action />
-        <Options options={options} />
-        <AddOption />
+        <Action
+          hasOptions={this.state.options.length > 0}
+          handlePick={this.handlePick}
+        />
+        <Options
+          options={this.state.options}
+          handleDeleteOptions={this.handleDeleteOptions}
+        />
+        <AddOption
+          handleAddOption={this.handleAddOption}
+        />
       </div>
     )
   }
@@ -30,19 +73,15 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.handlePick = this.handlePick.bind(this)
-  }
-  handlePick() {
-    alert('handlePick')
-  }
-
   render() {
     return (
       <div>
-        <button onClick={this.handlePick}>What should I do?</button>
+        <button
+          onClick={this.props.handlePick}
+          disabled={!this.props.hasOptions}
+        >
+          What should I do?
+        </button>
       </div>
     )
   }
@@ -51,12 +90,6 @@ class Action extends React.Component {
 class Options extends React.Component {
   constructor(props) {
     super(props)
-
-    this.handleRemoveAll = this.handleRemoveAll.bind(this)
-  }
-
-  handleRemoveAll() {
-    alert('handleRemoveAll')
   }
 
   render() {
@@ -64,7 +97,7 @@ class Options extends React.Component {
 
     return (
       <div>
-        <button onClick={this.handleRemoveAll}>Remove All</button>
+        <button onClick={this.props.handleDeleteOptions}>Remove All</button>
         {options.map((option, idx) =>
           <Option key={`option-${idx}`} optionText={option} />
         )}
@@ -87,27 +120,36 @@ class AddOption extends React.Component {
   constructor(props) {
     super(props)
 
+    this.state = {
+      error: undefined
+    }
+
     this.handleAddOption = this.handleAddOption.bind(this)
   }
 
   handleAddOption(e) {
     e.preventDefault()
 
-    const value = e.target.elements.option.value.trim()
+    const option = e.target.elements.option.value.trim()
+    const error = this.props.handleAddOption(option)
 
-    if (!!value) {
-      alert('handleAddOption')
-    }
+    this.setState(() => ({ error }))
   }
 
   render() {
     return (
-      <form onSubmit={this.handleAddOption}>
-        <input type="text" name="option"/>
-        <button>Add Option</button>
-      </form>
+      <div>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option"/>
+          <button>Add Option</button>
+        </form>
+      </div>
     )
   }
 }
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
+ReactDOM.render(
+  <IndecisionApp />,
+  document.getElementById('app')
+)
