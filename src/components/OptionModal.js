c71beb19365b6e36da422f7cdfe1ccab4dbd4ce4
armdong <armdong@163.com>
2018-03-06 12:43:50 +0800

    added react-modal

---
+++ src/components/OptionModal.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Modal from 'react-modal'
+
+const OptionModal = ({
+  selectedOption,
+  handleClearSelectedOption
+}) => (
+  <Modal
+    isOpen={!!selectedOption}
+    onRequestClose={handleClearSelectedOption}
+    contentLabel="Selected Option"
+  >
+    <h3>Selected Option</h3>
+    {!!selectedOption && <p>{selectedOption}</p>}
+    <button
+      onClick={handleClearSelectedOption}
+    >Okay</button>
+  </Modal>
+)
+
+export default OptionModal
