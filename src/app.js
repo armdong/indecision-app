a313b83e63cadab87a783cb522c403dc8ea4cd0f
armdong <armdong@163.com>
2018-03-06 10:11:31 +0800

    setting up webpack and babel

--- src/app.js
+++ src/app.js
@@ -1,14 +1,5 @@
-// import subtract, { square, add } from './utils'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-// console.log('app.js is running!')
-// console.log(square(4))
-// console.log(add(100, 23))
-// console.log(subtract(10, 2))
-
-
-
-import isSenior, { isAdult, canDrink } from './person'
-
-console.log(isAdult(17))
-console.log(canDrink(21))
-console.log(isSenior(65))
+const template = <p>This is JSX from webpack</p>
+ReactDOM.render(template, document.getElementById('app'))
